a20935144f54adf26f1d5abfbd0b862c44f6f6fe
Thulani Boyboy Ndlovu <tboyndlovu@gmail.com>
2019-12-22 11:47:59 +0200

    worked on data layer

---
+++ Data Layer/savy.data/Contexts/savyDbContext.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using savy.domain.Entities.Catalog
+
+namespace savy.data.Contexts
+{
+    public class savyDbContext : DbContext
+    {
+        public savyDbContext(DbContextOptions<savyDbContext> builder) : base(builder) {}
+
+        public Dbset<Product> products { get; set; }
+
+        proteted override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            //assign connection string
+            var conn = @"connection string";
+            builder.UseSqlServer(conn);
+        }
+    }
+}
---
+++ Test/savy.client.test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace savy.client.test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
